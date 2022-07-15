3ffd2e3bf350e902e7823739c76673ebd3fbb580
Jeff Oliver <jeffoliver3384@gmail.com>
2022-07-15 01:04:47 -0400

    wip: comments for a tweet

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,8 +8,9 @@ interface Props {
 }
 
 const Feed: React.FC<Props> = ({ tweets }) => {
+  console.log(tweets);
   return (
-    <div className="col-span-7 lg:col-span-5 border-x">
+    <div className="col-span-7 lg:col-span-5 border-x overflow-y-scroll">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import TimeAgo from "react-timeago";
-import { Tweet } from "../typings";
+import { Comment, Tweet } from "../typings";
+import { fetchComments } from "../utils/fetchComments";
 import TweetIcons from "./TweetIcons";
 
 interface Props {
@@ -8,14 +10,36 @@ interface Props {
 }
 
 const Tweet: React.FC<Props> = ({ tweet }) => {
-  const { profileImg, username, _createdAt, text, image } = tweet;
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  const {
+    _id,
+    profileImg,
+    username,
+    _createdAt,
+    text,
+    image
+  } = tweet;
+
   const userHandle = username.replace(/\s+/g, "").toLowerCase();
+
+  useEffect(() => {
+    (async function () {
+      const comments: Comment[] = await fetchComments(_id);
+
+      setComments(comments);
+    })()
+  }, [_id]);
+
+  console.log(comments);
 
   return (
     <div className="flex flex-col space-x-3 p-5 border-y border-gray-100">
       <div className='flex items-center space-x-3'>
-        <img
-          className='h-10 w-10 rounded-full object-cover'
+        <Image
+          width={40}
+          height={40}
+          className='rounded-full object-cover'
           src='/images/avatar-man-icon-profile-placeholder-260nw-1229859850-e1623694994111.jpg'
           alt=''
         />
@@ -34,12 +58,19 @@ const Tweet: React.FC<Props> = ({ tweet }) => {
 
       </div>
 
-      <div className="flex flex-col items-start pl-10">
-        <p className="pt-1">{text}</p>
+      <div className="flex flex-col items-start">
+        <p className="py-1 pl-10">{text}</p>
 
         {image && (
-          <img src={image} alt="" className="mt-5 mb-1 rounded-lg max-h-60 object-contain shadow-sm" />
+          <Image
+            width={1000}
+            height={1000}
+            src={image}
+            className="mt-5 mb-1 rounded-lg max-h-60 object-cover shadow-sm"
+            alt=""
+          />
         )}
+
       </div>
 
       <TweetIcons />
