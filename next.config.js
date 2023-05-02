d863844fa8319d9000aa2460ce15229374139b0c
Jeff Oliver <jeffoliver3384@gmail.com>
2023-05-02 06:08:59 -0400

    New create next app

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "www.storefrontier.com",
-      "yt3.ggpht.com",
-      "pbs.twimg.com",
-      "th.bing.com",
-      "cdn.sanity.io",
-      "firebasestorage.googleapis.com",
-    ],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- utils/fetchReplies.ts
+++
@@ -1,9 +0,0 @@
-// import { Reply } from "../types/typings";
-
-export const fetchReplies = async (tweetId: string) => {
-	const res = await fetch(`/api/getReplies?tweetId=${tweetId}`);
-
-	const replies = await res.json();
-
-	return replies;
-};
